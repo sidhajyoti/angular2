42b29c55ce6a17c19ab847e8541b1afaa6047101
sidhajyoti <sidhajyoti@users.noreply.github.com>
2016-09-08 00:31:29 +0530

    Add files via upload
    Changed the codes to when ccliked on login first compare the values from UI, then show the sucssess page.

--- login-form.component.ts
+++ login-form.component.ts
@@ -1,76 +1,33 @@
 import { Component } from '@angular/core';
-
-import { login }    from './login';
-
-import {bootstrap}&nbsp; from 'angular2/platform/browser';
-import {NGL_DIRECTIVES, provideNglConfig} from 'ng-lightning/ng-lightning';
+import { Router, ROUTER_DIRECTIVES } from '@angular/router';
+import {CORE_DIRECTIVES,FORM_DIRECTIVES} from '@angular/common';
+import { Http, Headers } from '@angular/http';
+import { login } from './login';
 
 @Component({
+
   selector: 'login-form',
-  directives: [NGL_DIRECTIVES],
   templateUrl: 'app/login-form.component.html',
+  directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES, FORM_DIRECTIVES]
+  
 })
 export class loginFormComponent {
 
-  powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
-
-  model = new login(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
-
-  submitted = false;
+	constructor (){
+		console.log("Inside Constructor....");
+	}
   
-    constructor (){
-        alert("Inside constructor");
-    }
-  onSubmit() { 
+  onSubmit(emailVal,pwdVal) { 
   
-  //console.log("Hi inside onSubmit method");
-  alert("HIii..");
-  this.submitted = true; 
-  
+	  console.log("Inside onSubmit()...."+ emailVal + " :: " + pwdVal);
+	  if(emailVal == "test@test.com" && pwdVal == "test"){
+		console.log("Success!!!");
+		this.submitted = true;
+		this.router.navigate(['/successPage']);
+	  }else{
+		console.log("Failed!!!");
+		alert("Invalid Username / Password, please check the credentials.");
+		this.submitted = false;
+	  }
   }
-
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
-  // Reset the form with a new login AND restore 'pristine' class state
-  // by toggling 'active' flag which causes the form
-  // to be removed/re-added in a tick via NgIf
-  // TODO: Workaround until NgForm has a reset method (#6822)
-  active = true;
-
-  newlogin() {
-    this.model = new login(42, '', '');
-    this.active = false;
-    setTimeout(() => this.active = true, 0);
-  }
-  //////// NOT SHOWN IN DOCS ////////
-
-  // Reveal in html:
-  //   Name via form.controls = {{showFormControls(loginForm)}}
-  showFormControls(form: any) {
-
-    return form && form.controls['name'] &&
-    form.controls['name'].value; // Dr. IQ
-  }
-
-  /////////////////////////////
-
 }
-
-export class Angular2Modal {
- 
-opened: boolean = false;
-size: string;
- 
-open(size: string) {
-this.size = size;
-this.opened = !this.opened;
-}
- 
-cancel() {
-this.opened = false;
-}
-}
- 
-bootstrap(Angular2Modal, [provideNglConfig()]);
