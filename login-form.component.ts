426c4fa09cfc817dc8799b58bc8b588f7f6e2086
sidhajyoti <sidhajyoti@users.noreply.github.com>
2016-09-11 00:23:35 +0530

    Add files via upload
    Changed the files as per the requirement and introduces the service

--- login-form.component.ts
+++ login-form.component.ts
@@ -1,33 +1,45 @@
 import { Component } from '@angular/core';
-import { Router, ROUTER_DIRECTIVES } from '@angular/router';
-import {CORE_DIRECTIVES,FORM_DIRECTIVES} from '@angular/common';
-import { Http, Headers } from '@angular/http';
+import {Router} from '@angular2/router';
 import { login } from './login';
 
-@Component({
+//Importing the AuthService, which was in service folder, to be used further.
+import {AuthService} from './services/login-form-authservice';
 
+
+@Component({
+	
   selector: 'login-form',
   templateUrl: 'app/login-form.component.html',
-  directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES, FORM_DIRECTIVES]
+  providers: [AuthService]
   
 })
+
 export class loginFormComponent {
 
-	constructor (){
-		console.log("Inside Constructor....");
-	}
+	//This will help to fetch the username and password from login form.
+	localUser = {
+        username: '',
+        password: ''
+    }
+	
+	//This constructor wll call the respective service AuthService.ts
+	constructor(private _service:AuthService, private _router: Router) {
+        console.log("Inside COnstructor..")
+    }
   
-  onSubmit(emailVal,pwdVal) { 
-  
-	  console.log("Inside onSubmit()...."+ emailVal + " :: " + pwdVal);
-	  if(emailVal == "test@test.com" && pwdVal == "test"){
-		console.log("Success!!!");
-		this.submitted = true;
-		this.router.navigate(['/successPage']);
-	  }else{
-		console.log("Failed!!!");
-		alert("Invalid Username / Password, please check the credentials.");
-		this.submitted = false;
-	  }
-  }
+  //This function will be called when login ll be clicked on the login-form.
+	login() {
+	
+		console.log("Inside LOgin()..")
+		// Here calling the service for validation of the given credentials.
+		this._service.loginfn(this.localUser).then((res) => {
+            if(res){
+				this._router.navigate(['Dashboard']);
+			}else{
+				alert("Invalid credentials!!");
+				console.log(res);
+			}
+        })
+    }
+	
 }
